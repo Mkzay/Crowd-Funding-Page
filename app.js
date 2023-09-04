de51b884d36a0c8df95e90d5c8b5e23faf0fa5d6
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-09-04 22:55:38 +0100

    Little bit more functionality

--- app.js
+++ app.js
@@ -1,13 +1,79 @@
-const navToggle = document.getElementById('nav-toggle');
-const navbarLinks = document.getElementById('navbar-links');
-const toggleIcon = document.getElementById('toggle-icon');
+document.addEventListener('DOMContentLoaded', function() {
 
-navToggle.addEventListener('click', function() {
-  navbarLinks.classList.toggle('hidden');
+  //Script for Navigation
+  const navToggle = document.getElementById('nav-toggle');
+  const navbarLinks = document.getElementById('navbar-links');
+  const toggleIcon = document.getElementById('toggle-icon');
+  
+  navToggle.addEventListener('click', function() {
+    navbarLinks.classList.toggle('hidden');
+  
+    if (navbarLinks.classList.contains('hidden')) {
+      toggleIcon.src = 'images/icon-hamburger.svg';
+    } else {
+      toggleIcon.src = 'images/icon-close-menu.svg';
+    }
+  });
+  
+  
+  // Script for Main Modal
+  let selected1 = document.querySelector('#bg-button1');
+  let borderChange1 = document.querySelector('#selected-bg1');
+  let modalContainer1 = document.getElementById('modal-container1');
+  
+  selected1.addEventListener('click', function(){
+      borderChange1.style.borderColor = 'hsl(176, 50%, 47%)';
+      borderChange1.style.borderWidth = '2px'
+      modalContainer1.style.display = 'block';
+  });
 
-  if (navbarLinks.classList.contains('hidden')) {
-    toggleIcon.src = 'images/icon-hamburger.svg';
-  } else {
-    toggleIcon.src = 'images/icon-close-menu.svg';
-  }
+  let selected2 = document.querySelector('#bg-button2');
+  let borderChange2 = document.querySelector('#selected-bg2');
+  let modalContainer2 = document.getElementById('modal-container2');
+  
+  selected2.addEventListener('click', function(){
+      borderChange2.style.borderColor = 'hsl(176, 50%, 47%)';
+      borderChange2.style.borderWidth = '2px'
+      modalContainer2.style.display = 'block';
+  });
+
+  let selected3 = document.querySelector('#bg-button3');
+  let borderChange3 = document.querySelector('#selected-bg3');
+  let modalContainer3 = document.getElementById('modal-container3');
+  
+  selected3.addEventListener('click', function(){
+      borderChange3.style.borderColor = 'hsl(176, 50%, 47%)';
+      borderChange3.style.borderWidth = '2px'
+      modalContainer3.style.display = 'block';
+  });
+
+
+  //Script for Tiny modal
+  let projectButton = document.querySelector('#project');
+  let closeBtn = document.querySelector('#close-btn');
+  let projectCard = document.querySelector('#project-card');
+  
+  projectButton.addEventListener('click', function(){
+      projectCard.style.display = 'block';
+  });
+  
+  closeBtn.addEventListener('click', function(){
+      projectCard.style.display = 'none';
+  });
+
+  //Script for Completion
+  let completeButton = document.querySelector('#complete-button1');
+  let thankYouModal = document.querySelector('#thank-you-modal');
+  let clearButton = document.querySelector('#clear-button');
+
+  completeButton.addEventListener('click', function(){
+  thankYouModal.style.display = 'block';
+  projectCard.style.display = 'none';
+
+  });
+
+  clearButton.addEventListener('click', function(){
+    thankYouModal.style.display = 'none';
+  });
+
 });
