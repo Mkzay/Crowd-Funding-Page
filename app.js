9382675ea42d182860a09549f3fc7317fcfa288b
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-09-07 19:44:43 +0100

    Final for now

--- app.js
+++ app.js
@@ -17,66 +17,81 @@ document.addEventListener('DOMContentLoaded', function() {
   
   
   // Script for Tiny Modals
-// Store references to the currently selected button and modal
-let currentSelectedButton = null;
-let currentModalContainer = null;
+// Function to deselect all buttons and hide all modal containers
+// Function to deselect all buttons and hide all modal containers
+function deselectAll() {
+  for (let i = 1; i <= 3; i++) {
+    let borderChange = document.querySelector(`#selected-bg${i}`);
+    let modalContainer = document.getElementById(`modal-container${i}`);
+    
+    borderChange.style.borderColor = '';
+    borderChange.style.borderWidth = '';
+    modalContainer.classList.add('custom-hidden');
+  }
+}
 
 // Loop through buttons with IDs like bg-button1, bg-button2, bg-button3
 for (let i = 1; i <= 3; i++) {
   let selectedButton = document.querySelector(`#bg-button${i}`);
   let borderChange = document.querySelector(`#selected-bg${i}`);
   let modalContainer = document.getElementById(`modal-container${i}`);
-
+  
   selectedButton.addEventListener('click', function () {
-    // Close the previously opened modal (if any)
-    if (currentModalContainer) {
-      currentModalContainer.style.display = 'none';
-    }
-
-    // Reset the border styles for all buttons
-    for (let j = 1; j <= 3; j++) {
-      let resetBorderChange = document.querySelector(`#selected-bg${j}`);
-      resetBorderChange.style.borderColor = '';
-      resetBorderChange.style.borderWidth = '';
-    }
-
+    // Deselect all buttons and hide all modal containers
+    deselectAll();
+    
     // Set the new current button and modal
-    currentSelectedButton = selectedButton;
-    currentModalContainer = modalContainer;
-
-    // Open the clicked modal and apply border styles
     borderChange.style.borderColor = 'hsl(176, 50%, 47%)';
     borderChange.style.borderWidth = '2px';
-    modalContainer.style.display = 'block';
+    modalContainer.classList.remove('custom-hidden');
   });
 }
 
-  //Script for Main modal
-  let projectButton = document.querySelector('#project');
-  let closeBtn = document.querySelector('#close-btn');
-  let projectCard = document.querySelector('#project-card');
-  
-  projectButton.addEventListener('click', function(){
-      projectCard.style.display = 'flex';
-  });
-  
-  closeBtn.addEventListener('click', function(){
-      projectCard.style.display = 'none';
-  });
+
+
+// Script for Main modal
+let projectButton = document.querySelector('#project');
+let closeBtn = document.querySelector('#close-btn');
+let projectCard = document.querySelector('#project-card');
+
+function openModal() {
+  projectCard.classList.remove('custom-hidden');
+}
+
+function closeModal() {
+  projectCard.classList.add('custom-hidden');
+}
+
+projectButton.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+
 
   //Script for Completion
-  let completeButton = document.querySelector('#complete-button1');
+  let completeButton1 = document.querySelector('#complete-button1');
   let thankYouModal = document.querySelector('#thank-you-modal');
   let clearButton = document.querySelector('#clear-button');
 
-  completeButton.addEventListener('click', function(){
-  thankYouModal.style.display = 'flex';
-  projectCard.style.display = 'none';
-
+  completeButton1.addEventListener('click', function(){
+    thankYouModal.classList.remove('custom-hidden');
   });
 
   clearButton.addEventListener('click', function(){
-    thankYouModal.style.display = 'none';
+    thankYouModal.classList.add('custom-hidden');
+  });
+
+  clearButton.addEventListener('click', closeModal);
+
+  let completeButton2 = document.querySelector('#complete-button2');
+
+  completeButton2.addEventListener('click', function(){
+    thankYouModal.classList.remove('custom-hidden');
+  });
+
+  let completeButton3 = document.querySelector('#complete-button3');
+
+  completeButton3.addEventListener('click', function(){
+    thankYouModal.classList.remove('custom-hidden');
   });
 
   //Script for bookmark
@@ -89,9 +104,7 @@ for (let i = 1; i <= 3; i++) {
   })
 
   //Script for Rewards Button
-
-
-// Function to scroll to a specific section in the modal
+ // Function to scroll to a specific section in the modal
 function scrollToSection() {
   // Specify the ID of the section you want to scroll to
   var sectionIdToScrollTo = "targetSection";
@@ -112,7 +125,7 @@ function scrollToSection() {
 let rewardButton1 = document.querySelector('#reward-button1');
 
 rewardButton1.addEventListener('click', function(){
-  projectCard.style.display = 'flex';
+  projectCard.classList.remove('custom-hidden');
 
   // Call the scrollToSection function here to scroll to the specified section
   scrollToSection();
@@ -123,7 +136,7 @@ function scrollToSection() {
   var sectionIdToScrollTo = "targetSection";
 
   // Get the target section element by its ID
-  var targetSection = document.getElementById('selected-bg2');
+  var targetSection = document.getElementById('selected-bg3');
 
   // Check if the target section exists before scrolling
   if (targetSection) {
@@ -138,7 +151,7 @@ function scrollToSection() {
 let rewardButton2 = document.querySelector('#reward-button2');
 
 rewardButton2.addEventListener('click', function(){
-  projectCard.style.display = 'flex';
+  projectCard.classList.remove('custom-hidden');
 
   // Call the scrollToSection function here to scroll to the specified section
   scrollToSection();
